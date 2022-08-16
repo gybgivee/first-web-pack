f05528a1c14fe957962f9d4fb7f0352b8fefb04d
gybgivee <kmcn0512@gmail.com>
2022-08-16 18:37:13 +0100

    update-gitignore

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+//use=> run from right to left , order is important! because we need css loader before style loader
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './index.html'
+    })
+  ]
+};
